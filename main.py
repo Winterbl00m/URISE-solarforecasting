3ab5f32ace9faf4c4e3e46f6029f6fd1ce2a13b8
Winterbl00m <60327852+Winterbl00m@users.noreply.github.com>
2021-06-18 15:50:54 -0500

    added main.py. Also added create_model function structure.

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers 
+
+def create_model(learning_rate, feature_layer):
+    """
+    Creates an ANN model
+
+    learning_rate: float. The learning rate of the model. Between 0 and 1
+    feature_layer: A tf layer.  Aka the input layer. The first layer of the model. 
+    """
+
+    # Add the layer containing the features to the model.
+    model.add(feature_layer)
+
+    #TODO: Define Hidden Layers
+
+    #TODO: Define output layer
+
+    #TODO: compile model
+
+    #TODO: when done delte pass and uncomment out
+    #return model
+    pass 
+
