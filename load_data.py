a0f56c53483d2f5a1780a9d7b205f7fdb373a779
aarish-lakhani <85258314+aarish-lakhani@users.noreply.github.com>
2021-07-12 12:28:13 -0500

    In progress (continued): working on loading weather data

--- load_data.py
+++ load_data.py
@@ -9,10 +9,12 @@ psds = psds[col_list]
 # psds = psds.loc[35032:69679]
 psds = psds.loc[35032:35047]
 psds = psds.rename(columns={"local_15min": "Time"})
-psds['Date/Time'] = pd.to_datetime(psds["Time"])
-# psds = psds.sort_values(by='Date/Time', ascending=True)
+psds['Time'] = pd.to_datetime(psds['Time'])
+psds.set_index('Time').resample('1T')
+psds = psds.sort_values(by='Time', ascending=True)
 psds.insert(loc=6, column="Sum of Power", value=psds.sum(axis=1))
 psds.fillna(0, inplace=True)
+print(psds)
 # psds.to_csv('preprocessingoutputfinal.csv', index=False)
 
 # 1/1/2018 (1:00 pm) - 12/31/2018 (11:45 pm)
@@ -20,16 +22,21 @@ psds.fillna(0, inplace=True)
 
 weatherfile = 'C:/Users/aaris/Downloads/ev_and_weather/ev_and_weather/weather.csv'
 
-wds = pd.read_csv(weatherfile)
+wds = pd.read_csv(weatherfile, parse_dates=['localhour'])
 weather_columns = ['localhour', 'temperature']
 wds = wds[weather_columns]
-wds = wds.loc[163375:163378]
-wpsds = pd.DataFrame(np.repeat(wds.values,4,axis=0))
+wds = wds.loc[163375:163379]
+wpsds = pd.DataFrame(np.repeat(wds.values, 4, axis=0))
 wpsds.columns = wds.columns
-wpsds['W-Date/Time'] = pd.to_datetime(wpsds["localhour"])
-# wpsds = wpsds.sort_values(by='W-Date/Time', ascending=True)
+pd.to_datetime(wpsds['localhour'])
+# print(wpsds)
+wpsds.set_index('localhour').resample('15T')
+# print(wpsds)
+# wpsds.resample('15Min')
+# wpsds = wpsds.sort_values(by='localhour', ascending=True)
+# wpsds = wpsds.asfreq('15min')
+# print(wpsds)
 # wpsds.to_csv('loaddataweathertest.csv', index=False)
 # psds['temperature'] = wpsds['temperature'].values
-wpsds.to_csv('loaddataweathertest.csv', index=False)
-psds.to_csv('loaddataweathertest1.csv', index=False)
+# psds.to_csv('loaddataweathertest1.csv', index=False)
 
