7ecfa43c010a357dfaabab38066069261d0e1b11
aarish-lakhani <85258314+aarish-lakhani@users.noreply.github.com>
2021-07-16 16:12:07 -0500

    Fixed timezone error and integrated solar

--- load_data.py
+++ load_data.py
@@ -4,19 +4,18 @@ import numpy as np
 filename = 'C:/Users/aaris/Downloads/15minute_data_austin.csv'
 
 psds = pd.read_csv(filename)
-col_list = ['local_15min', 'air1', 'clotheswasher1', 'dishwasher1', 'furnace1', 'refrigerator1']
+col_list = ['local_15min', 'air1', 'clotheswasher1', 'dishwasher1', 'furnace1', 'refrigerator1', 'solar']
 psds = psds[col_list]
 psds = psds.rename(columns={"local_15min": "Time"})
 psds = psds.loc[35032:69679]
-psds['Time'] = pd.to_datetime(psds['Time'], utc=True, errors='coerce')
-# to_datetime turns values into datetimeindex, required to remove timezone
-psds['Time'] = psds['Time'].dt.tz_localize(None)
-# tz_localize(None) removes timezone from Time
+psds['Time'] = psds['Time'].astype(str).str[:-6]
+# removes timezone from Time
+psds['Time'] = pd.to_datetime(psds['Time'], errors='coerce')
+# to_datetime turns values into datetimeindex, required to merge with weather data
 psds = psds.sort_values(by='Time', ascending=True)
 # sorts values in ascending order of Time - data was not completely sorted originally
 psds.fillna(0, inplace=True)
-psds.insert(loc=6, column="Sum of Power", value=psds.sum(axis=1))
-# psds.to_csv('preprocessingoutputfinal.csv', index=False)
+psds.insert(loc=7, column="Sum of Power", value=psds.drop('solar', axis=1).sum(axis=1))
 
 # 1/1/2018 (1:00 pm) - 12/31/2018 (11:45 pm)
 # [163375:171273]
@@ -33,4 +32,4 @@ wds['Time'] = pd.to_datetime(wds['Time'], errors='coerce')
 
 combined = psds.merge(wds, how='outer')
 combined = combined.fillna(method='ffill')
-combined.to_csv('TESTloadandweatherdata.csv', index=False)
+combined.to_csv('solar_load_weatherdata.csv', index=False)
