32854e0ccc61209bae92fcb1fd6e675102af116f
Winterbl00m <60327852+Winterbl00m@users.noreply.github.com>
2021-07-19 09:58:30 -0500

    Added solar to model

--- take_two.py
+++ take_two.py
@@ -75,6 +75,7 @@ def create_dataset(df, indexes):
     output_df.pop('Time')
     output_df.pop('Sum of Power')
 
+    print(output_df.head())
     return input_df, output_df 
 
 
@@ -97,8 +98,10 @@ def create_model():
     y3_output = Dense(units='1', name='dishwasher1')(first_hidden_layer)
     y4_output = Dense(units='1', name='furnace1')(first_hidden_layer)
     y5_output = Dense(units='1', name='refrigerator1')(first_hidden_layer)
+    y6_output = Dense(units='1', name='solar')(first_hidden_layer)
+
     #create tf model object
-    model = Model(inputs=input_layer,outputs=[y1_output, y2_output, y3_output, y4_output, y5_output])
+    model = Model(inputs=input_layer,outputs=[y1_output, y2_output, y3_output, y4_output, y5_output, y6_output])
 
     # Specify the optimizer, and compile the model with loss functions for both outputs
     optimizer = tf.keras.optimizers.SGD(learning_rate=0.001)
@@ -107,19 +110,21 @@ def create_model():
                   'clotheswasher1': 'mse',
                   'dishwasher1' : 'mse',
                   'furnace1' : 'mse',
-                  'refrigerator1' : 'mse'},
+                  'refrigerator1' : 'mse',
+                  'solar' : 'mse'},
 
                   metrics={'air1': tf.keras.metrics.RootMeanSquaredError(),
                   'clotheswasher1': tf.keras.metrics.RootMeanSquaredError(),
                   'dishwasher1' : tf.keras.metrics.RootMeanSquaredError(),
                   'furnace1' : tf.keras.metrics.RootMeanSquaredError(),
-                  'refrigerator1' : tf.keras.metrics.RootMeanSquaredError()} )
+                  'refrigerator1' : tf.keras.metrics.RootMeanSquaredError(),
+                  'solar' : tf.keras.metrics.RootMeanSquaredError()} )
 
     return model
 
 
 #reads data from the preprocessed csv file
-df = pd.read_csv('preprocessingoutputfinal.csv')
+df = pd.read_csv('solar_load_weatherdata.csv')
 
 train_frac = .6
 val_frac = .2
@@ -134,8 +139,8 @@ val_x, val_y = create_dataset(df, indexes = val_indexes)
 # list_of_outputs = ['air1', 'clotheswasher1', 'dishwasher1', 'furnace1', 'refrigerator1']
 
 #Turns output data from a dataframe to five arrays
-train_y = train_y.pop('air1') , train_y.pop('clotheswasher1'), train_y.pop('dishwasher1'), train_y.pop('furnace1'), train_y.pop('refrigerator1')
-val_y = val_y.pop('air1') , val_y.pop('clotheswasher1'), val_y.pop('dishwasher1'), val_y.pop('furnace1'), val_y.pop('refrigerator1')
+train_y = train_y.pop('air1') , train_y.pop('clotheswasher1'), train_y.pop('dishwasher1'), train_y.pop('furnace1'), train_y.pop('refrigerator1'), train_y.pop('solar')
+val_y = val_y.pop('air1') , val_y.pop('clotheswasher1'), val_y.pop('dishwasher1'), val_y.pop('furnace1'), val_y.pop('refrigerator1'), val_y.pop('solar')
 
 #Create Model
 model = create_model()
@@ -172,6 +177,7 @@ clotheswasher_rmse = history.history['clotheswasher1_root_mean_squared_error']
 dishwasher_rmse = history.history['dishwasher1_root_mean_squared_error']
 furnace_rmse = history.history['furnace1_root_mean_squared_error']
 refrigerator_rmse = history.history['refrigerator1_root_mean_squared_error']
+solar_rmse = history.history['solar_root_mean_squared_error']
 
 epochs = range(1, len(air_rmse) + 1)
 
@@ -182,6 +188,7 @@ plt.plot(epochs, clotheswasher_rmse, color = 'green', label='Clotheswaher RMSE')
 plt.plot(epochs, dishwasher_rmse, color = 'red', label='Dishwasher RMSE')
 plt.plot(epochs, furnace_rmse, color = 'purple', label='Furnace RMSE')
 plt.plot(epochs, refrigerator_rmse, color = 'orange', label='Refrigerator RMSE')
+plt.plot(epochs, solar_rmse, color = 'gold', label='Solar RMSE')
 
 plt.title('Training RMSE over Epochs')
 plt.xlabel('Epochs')
@@ -196,6 +203,7 @@ val_clotheswasher_rmse = history.history['val_clotheswasher1_root_mean_squared_e
 val_dishwasher_rmse = history.history['val_dishwasher1_root_mean_squared_error']
 val_furnace_rmse = history.history['val_furnace1_root_mean_squared_error']
 val_refrigerator_rmse = history.history['val_refrigerator1_root_mean_squared_error']
+val_solar_rmse = history.history['val_solar_root_mean_squared_error']
 
 epochs = range(1, len(val_air_rmse) + 1)
 
@@ -206,6 +214,7 @@ plt.plot(epochs, val_clotheswasher_rmse, color = 'green', label='Clotheswaher RM
 plt.plot(epochs, val_dishwasher_rmse, color = 'red', label='Dishwasher RMSE')
 plt.plot(epochs, val_furnace_rmse, color = 'purple', label='Furnace RMSE')
 plt.plot(epochs, val_refrigerator_rmse, color = 'orange', label='Refrigerator RMSE')
+plt.plot(epochs, val_solar_rmse, color = 'gold', label='Solar RMSE')
 
 plt.title('Validation RMSE over Epochs')
 plt.xlabel('Epochs')
