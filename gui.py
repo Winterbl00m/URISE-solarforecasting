6e2cfe7d0601739a659555d9abbd7d13a9851ffe
haileyferrel <85899092+haileyferrel@users.noreply.github.com>
2021-07-27 11:51:09 -0400

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+#Open window
+window = tk.Tk()
+
+#Asking Question
+label = tk.Label(text="Which appliances are you inquiring about?", fg="black", bg="white")
+#Doesn't quotation marks comment things out? That is all commented out...
+
+window.rowconfigure(7, minisize=50, weight=1)
